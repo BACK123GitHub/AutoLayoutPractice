6b585a203f26723decf9fd448848e9f911cacb3b
BACK123GitHub <724829118@qq.com>
2019-05-29 09:14:51 +0800

    创建了基础tableVIew

---
+++ Podfile
@@ -0,0 +1,7 @@
+platform:ios,'9.3'
+use_frameworks!
+
+target 'autoLayoutTest' do
+	pod 'SDWebImage', '~>3.7.6'
+	pod 'Masonry', '~>1.0.0'
+end
---
+++ autoLayoutTest/BKTableViewController.h
@@ -0,0 +1,19 @@
+//
+//  BKTableViewController.h
+//  autoLayoutTest
+//
+//  Created by 孟庆儒 on 2019/5/28.
+//  Copyright © 2019 孟庆儒. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BKTableViewController : UITableViewController
+
+@property(nonatomic,strong) UITableView* bktableView;
+
+@end
+
+NS_ASSUME_NONNULL_END
